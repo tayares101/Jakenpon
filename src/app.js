a1fbe3a588bb6518521f03aede78ce69d57a495b
Aksel <kuruaksel@gmail.com>
2023-04-23 15:16:26 +0200

    [Add] First commit => For now index.html done

---
+++ src/app.js
@@ -0,0 +1,32 @@
+function changeHandStand(stand, playerId) {
+    document.getElementById("")
+}
+
+function arcNemesisHandStand() {
+    var handStand = "";
+    
+    switch (Math.floor(Math.random() * 3)) {
+        case '0':
+            handStand = "Rock";
+            break;
+        case '1':
+            handStand = "Paper";
+            break;
+        default:
+            handStand = "Scissors";
+    }
+
+    return handStand;
+}
+
+function updateScore(roundWinner) {
+    if (roundWinner == '0') {
+        
+    } else {
+
+    }
+}
+
+function fetchAllData() {
+
+}
